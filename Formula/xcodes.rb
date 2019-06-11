d4a1001208f73138b358b1d21bb3868a2e758976
Brandon Evans <brandon@brandonevans.ca>
2019-06-11 07:46:17 -0600

    Release xcodes 0.5.0

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,13 +1,13 @@
 class Xcodes < Formula
   desc "Manage the Xcodes installed on your Mac"
   homepage "https://github.com/RobotsAndPencils/xcodes"
-  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.4.0"
+  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.5.0"
   head "https://github.com/RobotsAndPencils/xcodes.git"
 
   bottle do
-    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.4.0'
+    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.5.0'
     cellar :any_skip_relocation
-    sha256 "412c5571077c4391340b57b6539e03183a090ef9ca9d1b515e5a7b949f2adf11" => :mojave
+    sha256 "2cd9927dc7d9f6a81e71d71db919eddf0c5e590e9e727f95c323deaddeec8dc9" => :mojave
   end
 
   def install
