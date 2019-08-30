80e0c56ac6d2f3f18d779a03c9922e02ed3ea1ed
Brandon Evans <brandon@brandonevans.ca>
2019-08-29 21:16:13 -0600

    Release xcodes 0.7.0

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,13 +1,13 @@
 class Xcodes < Formula
   desc "Manage the Xcodes installed on your Mac"
   homepage "https://github.com/RobotsAndPencils/xcodes"
-  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.6.0"
+  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.7.0"
   head "https://github.com/RobotsAndPencils/xcodes.git"
 
   bottle do
-    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.6.0'
+    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.7.0'
     cellar :any_skip_relocation
-    sha256 "f05d9bae064a9dc63488c07ce136acb592d1f252bec2604522231c069fef93b8" => :mojave
+    sha256 "3d5bbbe58f92f375ef7065fbfeafe85c008d27b9ac1ef20e98bcf5b6b311ba97" => :mojave
   end
 
   def install
