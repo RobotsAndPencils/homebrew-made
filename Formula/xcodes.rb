19ff7ffe40db53986dc2c6def9be75de38a12458
Brandon Evans <brandon@brandonevans.ca>
2020-08-28 22:01:04 -0600

    Release xcodes 0.11.0

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,13 +1,13 @@
 class Xcodes < Formula
   desc "Manage the Xcodes installed on your Mac"
   homepage "https://github.com/RobotsAndPencils/xcodes"
-  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.10.0"
+  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.11.0"
   head "https://github.com/RobotsAndPencils/xcodes.git"
 
   bottle do
-    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.10.0'
+    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.11.0'
     cellar :any_skip_relocation
-    sha256 "507cf1b78aab2a2ab4f46032431d7ea58e2e6e2be2831da42912325cb3b16dc7" => :mojave
+    sha256 "63ffea0354a93096d059ec21f3d50ac23dc15aa11b47c2c9174677ed9afdd193" => :mojave
   end
 
   def install
