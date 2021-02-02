f94247479cec56e17138abdd942ff669b0c76843
Brandon Evans <brandon@brandonevans.ca>
2021-02-01 21:47:15 -0700

    Release xcodes 0.16.0

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,13 +1,13 @@
 class Xcodes < Formula
-  desc "Manage the Xcodes installed on your Mac"
+  desc "Install and switch between multiple versions of Xcode"
   homepage "https://github.com/RobotsAndPencils/xcodes"
-  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.15.0"
+  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.16.0"
   head "https://github.com/RobotsAndPencils/xcodes.git"
 
   bottle do
-    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.15.0'
+    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.16.0'
     cellar :any_skip_relocation
-    sha256 "48a3c95e144f9032a6c9319e2081a2a44842296f59b7e78600ebe4037e2bf42e" => :mojave
+    sha256 "c4f625edb2f3aa8b5669e8cf5ba75430585e0d76f7632a37f921a2ecfaf8d2ff" => :mojave
   end
 
   def install
