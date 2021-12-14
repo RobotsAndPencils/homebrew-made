01002bee2aa30ef5914c90ecb4d075410fec3020
Matt Kiazyk <mattkiazyk@gmail.com>
2021-12-14 09:41:21 -0600

    Update xcodes to 0.18.0

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,12 +1,12 @@
 class Xcodes < Formula
   desc "Install and switch between multiple versions of Xcode"
   homepage "https://github.com/RobotsAndPencils/xcodes"
-  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.17.0"
+  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.18.0"
   head "https://github.com/RobotsAndPencils/xcodes.git"
 
   bottle do
-    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.17.0'
-    sha256 cellar: :any_skip_relocation, mojave: "bd92a58f7a53091d3c40657f82508b277f57e3827aead6eb3f253475854a89b0"
+    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.18.0'
+    sha256 cellar: :any_skip_relocation, mojave: "5daf2e30085ce4da11b9c06a054a780149513ba0c052868c927264ace374ae68"
   end
 
   def install
