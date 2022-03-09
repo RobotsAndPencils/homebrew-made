fe1a4ace6425df495a44fdfa229af0e657a2107b
Matt Kiazyk <mattkiazyk@gmail.com>
2022-03-09 08:27:13 -0600

    Updates xcodes to 0.20.0

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,12 +1,12 @@
 class Xcodes < Formula
   desc "Install and switch between multiple versions of Xcode"
   homepage "https://github.com/RobotsAndPencils/xcodes"
-  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.19.0"
-  head "https://github.com/RobotsAndPencils/xcodes.git"
+  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.20.0"
+  head "https://github.com/RobotsAndPencils/xcodes.git", branch: "main"
 
   bottle do
-    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.19.0'
-    sha256 cellar: :any_skip_relocation, mojave: "23cc91ad565e090b85921c51dfa7f709dde1360ebbfb118d1ac605ebd57c7789"
+    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.20.0'
+    sha256 cellar: :any_skip_relocation, mojave: "acd0717e5083634c472d17d7b041265f34b197164ecfcf513b812d77ff978c6d"
   end
 
   def install
