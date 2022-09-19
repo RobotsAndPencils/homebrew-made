9c05e5f4fc165f34869558c3d119d696114aa56e
Matt Kiazyk <mattkiazyk@gmail.com>
2022-09-18 22:31:39 -0500

    Update Xcodes to 1.0.0

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,12 +1,13 @@
 class Xcodes < Formula
   desc "Install and switch between multiple versions of Xcode"
   homepage "https://github.com/RobotsAndPencils/xcodes"
-  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "0.20.0"
+  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "1.0.0"
   head "https://github.com/RobotsAndPencils/xcodes.git", branch: "main"
 
   bottle do
-    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/0.20.0'
-    sha256 cellar: :any_skip_relocation, mojave: "acd0717e5083634c472d17d7b041265f34b197164ecfcf513b812d77ff978c6d"
+    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/1.0.0'
+    sha256 cellar: :any_skip_relocation, mojave: "23f4ee4ecac29c8816011895d18da9e78f2ec1fa3184489809d6e126039fa499"
+    sha256 cellar: :any_skip_relocation, arm64_mojave: "23f4ee4ecac29c8816011895d18da9e78f2ec1fa3184489809d6e126039fa499"
   end
 
   def install
