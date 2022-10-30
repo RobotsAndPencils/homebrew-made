dcb3f0b1cea664507d3128203d3df7369f25b556
Matt Kiazyk <mattkiazyk@gmail.com>
2022-10-29 22:31:08 -0500

    Xcodes 1.1

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,13 +1,13 @@
 class Xcodes < Formula
   desc "Install and switch between multiple versions of Xcode"
   homepage "https://github.com/RobotsAndPencils/xcodes"
-  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "1.0.0"
+  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "1.1.0"
   head "https://github.com/RobotsAndPencils/xcodes.git", branch: "main"
 
   bottle do
-    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/1.0.0'
-    sha256 cellar: :any_skip_relocation, mojave: "23f4ee4ecac29c8816011895d18da9e78f2ec1fa3184489809d6e126039fa499"
-    sha256 cellar: :any_skip_relocation, arm64_mojave: "23f4ee4ecac29c8816011895d18da9e78f2ec1fa3184489809d6e126039fa499"
+    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/1.1.0'
+    sha256 cellar: :any_skip_relocation, mojave: "21a9c1c6d999623e92d1c9144c1bef9616509bcb5d60c0cd8603df5060b50713"
+    sha256 cellar: :any_skip_relocation, arm64_mojave: "21a9c1c6d999623e92d1c9144c1bef9616509bcb5d60c0cd8603df5060b50713"
   end
 
   def install
