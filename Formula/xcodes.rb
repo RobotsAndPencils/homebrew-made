f3bbb413b422f22497c938de11bc7459899e9bce
Matt Kiazyk <mattkiazyk@gmail.com>
2023-03-03 19:47:33 -0600

    Update Xcodes CLI to 1.3.0

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,13 +1,13 @@
 class Xcodes < Formula
   desc "Install and switch between multiple versions of Xcode"
-  homepage "https://github.com/RobotsAndPencils/xcodes"
-  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "1.2.0"
+  homepage "https://www.xcodes.app"
+  url "https://github.com/RobotsAndPencils/xcodes.git", tag: "1.3.0"
   head "https://github.com/RobotsAndPencils/xcodes.git", branch: "main"
 
   bottle do
-    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/1.2.0'
-    sha256 cellar: :any_skip_relocation, mojave: "94b823a331daa11f526bf15873284d8c0dbd0843c334644397bec82708474bed"
-    sha256 cellar: :any_skip_relocation, arm64_mojave: "94b823a331daa11f526bf15873284d8c0dbd0843c334644397bec82708474bed"
+    root_url 'https://github.com/RobotsAndPencils/xcodes/releases/download/1.3.0'
+    sha256 cellar: :any_skip_relocation, mojave: "f3908cec47dad544712e15133c713fde24f51ccd463aa582e856852be9bdb08e"
+    sha256 cellar: :any_skip_relocation, arm64_mojave: "f3908cec47dad544712e15133c713fde24f51ccd463aa582e856852be9bdb08e"
   end
 
   def install
